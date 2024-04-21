3c0b00eed524e1577169119b9f68a0281a57cbd7
joepklaassen <joep.klaassen9@gmail.com>
2024-04-21 14:23:10 +0200

    Extend default tailwind theme

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -2,21 +2,20 @@
 export default {
   content: ["./src/**/*.{vue,ts,html}"],
   theme: {
-    fontFamily: {
-      base: ["Inter", "Arial", "sans-serif"],
-    },
-    colors: {
-      transparent: "transparent",
-      green: "var(--green)",
-      white: "var(--white)",
-      "white-2": "var(--white-2)",
-      "light-grey": "var(--light-grey)",
-      "dark-grey": "var(--dark-grey)",
-      "dark-grey-30": "var(--dark-grey-30)",
-      "dark-grey-80": "var(--dark-grey-80)",
-      red: "var(--red)",
-    },
     extend: {
+      fontFamily: {
+        base: ["Inter", "Arial", "sans-serif"],
+      },
+      colors: {
+        green: "var(--green)",
+        white: "var(--white)",
+        "white-2": "var(--white-2)",
+        "light-grey": "var(--light-grey)",
+        "dark-grey": "var(--dark-grey)",
+        "dark-grey-30": "var(--dark-grey-30)",
+        "dark-grey-80": "var(--dark-grey-80)",
+        red: "var(--red)",
+      },
       padding: {
         default: "var(--padding-default)",
       },
